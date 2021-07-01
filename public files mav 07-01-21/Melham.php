9fc5e40d834a5d6c0f841861e74b5370996b1735
Weyv <86697947+Weyv@users.noreply.github.com>
2021-06-30 23:10:16 -0700

    Add files via upload

---
+++ public files mav 07-01-21/Melham.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include 'includes/head.php'; ?>
+</head>
+<body>
+
+	<?php include 'includes/navbar.php'; ?>
+
+		<!--- Image Slider -->
+	<div class="carousel slide" data-ride="carousel">
+		<div class="carousel-inner">
+			<div class="carousel-item active"><img src="img/MelhamPageBG.png"></div>
+		</div>
+	</div>
+	<!--- End Image Slider -->
+
+	<div class="row mt-5">
+	   	<div class="col-12 text-center mt-5">
+	   	   	<h3>About us</h3>
+	   	</div>
+	</div>
+
+	<!--- Two Column Section -->
+	<div class="container py-3">
+		<div class="row justify-content-center py-5">
+			<div class="col-lg-6">
+				<font size = "3">MELHAM CONSTRUCTION CORPORATION (MCC), first registered on February 2020 in the Philippine government situated at no.27 commonwealth avenue Diliman north Fairview Quezon City.</font>
+				<p>The main goal is to cater the growing demand of industrialization of the country.<p>
+				<p>Following the trends and demand of latest innovation in construction engineering, the company is committed in providing services following its core values.<p>			
+				<p>Melham Construction Corporation (MCC) cater broader scope dealing with vertical project such (Structural, Fit out works), Horizontal Project such (Roads, Highways and bridges), Telecommunication such (Cell site erection) and Technology such (Installation of fiber optics, SLI and ODN).</p><a class="btn btn-purple btn-lg" href="#">Melham Main Page</a>
+			</div>
+			<div class="col-lg-5"><img class="img-fluid" src="img/MelhamAboutUs.png"></div>
+		</div>
+	</div>
+	<!--- End Two Column Section -->
+
+	<!--- Start Jumbotron -->
+	<div class="jumbotron">
+		<div class="container">
+			<h2 class="text-center pt-5 pb-3">BUILT WITH THE BEST</h2>
+			<div class="row justify-content-center text-center">
+				<div class="col-10 col-md-4">
+					<div class="feature">
+						<img src="img/html5.png">
+						<h3>HTML5</h3>
+						<p>HTML5 is the fifth and current major version of the HTML standard, and subsumes XHTML.</p>
+					</div>
+				</div>
+				<div class="col-10 col-md-4">
+					<div class="feature">
+						<img src="img/bootstrap4.png">
+						<h3>BOOTSTRAP 4</h3>
+						<p>Bootstrap is an open-source front-end library with HTML and CSS based designs.</p>
+					</div>
+				</div>
+				<div class="col-10 col-md-4">
+					<div class="feature">
+						<img src="img/css3.png">
+						<h3>CSS3</h3>
+						<p>CSS3 is the latest evolution of the Cascading Style Sheets language and aims at extending CSS2.</p>
+					</div>
+				</div>
+			</div><!--- End Row -->
+		</div><!--- End Container -->
+	</div>
+	<!--- End Jumbotron -->
+
+<!--- Melham Button w link -->
+	<div class="container">
+		<div class="row justify-content-center text-center">
+			<div class="col-10 py-5">
+				<h2>Melham Construction Corporation</h2>
+				<p class="lead">Where We Build Your Visions.</p><a class="btn btn-purple btn-lg" href="https://melhamconstruction.ph/" target="_blank">Visit us!</a>
+			</div>
+		</div>
+	</div>
+
+	<!--- End Melham Button w link -->
+
+
+	<?php include 'includes/footer.php'; ?>
+
+	<?php include 'includes/scripts.php'; ?>
+
+</body>
+</html>
---
+++ public files mav 07-01-21/Visvis.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include 'includes/head.php'; ?>
+</head>
+<body>
+
+	<?php include 'includes/navbar.php'; ?>
+
+	<div class="row mt-5">
+	   	<div class="col-12 text-center mt-5">
+	   	   	<h3>Visvis</h3>
+	   	</div>
+	</div>
+
+	<!--- Start Jumbotron -->
+	<div class="jumbotron">
+		<div class="container">
+			<h2 class="text-center pt-5 pb-3">BUILT WITH THE BEST</h2>
+			<div class="row justify-content-center text-center">
+				<div class="col-10 col-md-4">
+					<div class="feature">
+						<img src="img/html5.png">
+						<h3>HTML5</h3>
+						<p>HTML5 is the fifth and current major version of the HTML standard, and subsumes XHTML.</p>
+					</div>
+				</div>
+				<div class="col-10 col-md-4">
+					<div class="feature">
+						<img src="img/bootstrap4.png">
+						<h3>BOOTSTRAP 4</h3>
+						<p>Bootstrap is an open-source front-end library with HTML and CSS based designs.</p>
+					</div>
+				</div>
+				<div class="col-10 col-md-4">
+					<div class="feature">
+						<img src="img/css3.png">
+						<h3>CSS3</h3>
+						<p>CSS3 is the latest evolution of the Cascading Style Sheets language and aims at extending CSS2.</p>
+					</div>
+				</div>
+			</div><!--- End Row -->
+		</div><!--- End Container -->
+	</div>
+	<!--- End Jumbotron -->
+		<!--- Visvis Logistic and Services Button w link -->
+	<div class="container">
+		<div class="row justify-content-center text-center">
+			<div class="col-10 py-5">
+				<h2>Visvis Logistic and Services</h2>
+				<p class="lead">Visvis Logistics Services is a premier cargo delivery services and passenger transport that caters Door to Door at any point of the country.</p><a class="btn btn-purple btn-lg" href="https://www.facebook.com/visvislogisticservices/" target="_blank">Visit us!</a>
+			</div>
+		</div>
+	</div>
+	<!--- End Visvis Logistic and Services Button w link -->
+
+	<!--- Visvis Visvis Travel and Tours Button w link -->
+	<div class="container">
+		<div class="row justify-content-center text-center">
+			<div class="col-10 py-5">
+				<h2>Visvis Travel and Tours</h2>
+				<p class="lead">Visvis Travel & Tours offers transportation, accommodation and other travel services having a unique never forgettable experience. Pacing our minds connected with vacations and holiday-pleasure.</p><a class="btn btn-purple btn-lg" href="https://www.facebook.com/visvistraveltours/" target="_blank">Visit us!</a>
+			</div>
+		</div>
+	</div>
+	<!--- End Visvis Travel and Tours Button w link -->
+
+	<!--- Two Column Section -->
+	<div class="container py-3">
+		<div class="row justify-content-center py-5">
+			<div class="col-lg-6">
+				<h3 class="pb-4">Learn to build Bootstrap websites</h3>
+				<p>The columns will automatically stack on top of each other when the screen is less than 992px wide.</p>
+				<p>Resize the browser window to see the effect. Responsive web design has become more important as the amount of mobile traffic now accounts for more than half of total internet traffic.</p><a class="btn btn-purple btn-lg" href="#">Bootstrap Course</a>
+			</div>
+			<div class="col-lg-6"><img class="img-fluid" src="img/responsive.png"></div>
+		</div>
+	</div>
+	<!--- End Two Column Section -->
+
+
+	<?php include 'includes/footer.php'; ?>
+
+	<?php include 'includes/scripts.php'; ?>
+
+</body>
+</html>
